10de277d116e662361fa745adff93d96c156cbba
Miguel Gutierrez <mgutierrezb@ucenfotec.ac.cr>
2020-11-07 17:02:59 -0600

    final

---
+++ Lab6/src/main/java/com/cenfotec/journal/web/JournalController.java
@@ -0,0 +1,24 @@
+package com.cenfotec.journal.web;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.cenfotec.journal.domain.Journal;
+import com.cenfotec.journal.service.JournalService;
+
+import java.text.ParseException;
+
+@Controller
+public class JournalController {
+    @Autowired
+    JournalService journalService;
+    @RequestMapping("/")
+    public String index(Model model) throws ParseException {
+        model.addAttribute("journal",
+                journalService.getAllJournals());
+        Journal newEntry = new Journal("Hola Mundo", "un saludo",
+                "07/15/2017");
+        journalService.saveJournal(newEntry);
+        return "index";
+    }
+}
